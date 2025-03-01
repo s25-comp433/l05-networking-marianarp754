246c3b0c5c580a64c1a3d4069e4f398285a742a5
Mariana <marianarp754@gmail.com>
2025-03-01 16:20:51 -0500

    Progress on L05

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,15 +7,63 @@
 
 import SwiftUI
 
+struct Result: Codable {
+    var id: Int
+    var team: String
+    var date: String
+    var opponent: String
+    var score: gameScores
+    var isHomeGame: Bool
+}
+
+struct gameScores: Codable {
+    var unc: Int
+    var opponent: Int
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(results, id: \.id) { item in
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("\(item.team) vs. \(item.opponent)")
+                        Spacer()
+                        Text("\(item.score.unc) - \(item.score.opponent)")
+                    }
+                    .font(.headline)
+                    HStack {
+                        Text(item.date)
+                        Spacer()
+                        if item.isHomeGame {
+                            Text("Home")
+                        } else {
+                            Text("Away")
+                        }
+                    }
+                    .foregroundStyle(.secondary)
+                }
+            }
+            .navigationTitle("UNC Basketball")
+            .task {
+                await loadData()
+            }
         }
-        .padding()
+    }
+    func loadData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode([Result].self, from: data) {
+                results = decodedResponse
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
